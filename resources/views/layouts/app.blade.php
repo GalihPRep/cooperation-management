1d2a2861bdaae3a7f2c2b2b3de17e234c396ff41
Galih <galihpri99@gmail.com>
2026-08-12 08:55:06 +0700

    Once again fixed the issue with the dropdown menu.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <title>Manajemen Kerjasama</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    
+    <!-- Load Vite CSS/JS -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
@@ -96,8 +98,6 @@
         </div>
     </footer>
 
-    <!-- Bootstrap 5 JS Bundle (includes Popper.js) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     @stack("scripts")
 </body>
 
